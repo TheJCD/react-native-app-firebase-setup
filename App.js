7ab8679e9c4c6b09001248a2d30c2673d910d677
James C <hi@jamescleworth.com>
2024-04-17 21:14:32 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,29 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
-
-  //import { initializeApp } from 'firebase/app';
-  import { initializeApp } from '@react-native-firebase/app';
-
-
-// if (!firebase.apps.length) {
-//     firebase.initializeApp(firebaseConfig);
-//   }
-
-  // Check if Firebase is initialized
-  //console.log('Firebase app:', firebase.app().name);
-
-
+import { initializeApp } from '@react-native-firebase/app';
 
 // Initialize Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyA2cFwjX3L8KLHws5YCqRwAPi2d7zx42Ec",
-  authDomain: "findmypt-45f51.firebaseapp.com",
-  projectId: "findmypt-45f51",
-  storageBucket: "findmypt-45f51.appspot.com",
-  messagingSenderId: "448644522866",
-  appId: "1:448644522866:web:93dddc43ed502bedc273eb",
-  databaseURL: ""
+  apiKey: "xxx-apikey-xxx",
+  authDomain: "xxxxxx.firebaseapp.com",
+  projectId: "xxxxx-45f51",
+  storageBucket: "xxxxx.appspot.com",
+  messagingSenderId: "xxxxx",
+  appId: "xxxxx",
+  databaseURL: "xxxx"
 };
 
 
